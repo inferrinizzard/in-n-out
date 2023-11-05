2ee0e5b4c2e33911e3ce14caf8c56e906af710d6
inferrinizzard <inferrinizzard@gmail.com>
2023-11-05 11:35:09 -0800

    configure babel tree shaking

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  const disableImportExportTransform = true;
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      {
+        native: {
+          disableImportExportTransform,
+        },
+        web: {
+          disableImportExportTransform,
+        },
+      },
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
