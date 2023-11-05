350604fdf17e2cbba1272a85942790d757473cf2
inferrinizzard <inferrinizzard@gmail.com>
2023-11-05 11:38:19 -0800

    add theme and provider

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { MD3LightTheme as DefaultTheme,  } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+
+export default theme;
