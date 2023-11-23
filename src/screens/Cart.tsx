19efbb72832b3117481c1372f17d5c96d9496588
inferrinizzard <inferrinizzard@gmail.com>
2023-11-22 19:26:40 -0800

    add basic text cart

--- src/screens/Cart.tsx
+++ src/screens/Cart.tsx
@@ -1,18 +1,30 @@
-import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { FlatList, View } from 'react-native';
+import { Text } from 'react-native-paper';
 
 import { type TabScreenProps } from './routes';
+
+import { useAppSelector } from '../redux/store';
+import { selectItems } from '../redux/slices/orderSlice';
 
 export interface CartProps {}
 
 const Cart: React.FC<CartProps & TabScreenProps<'Cart'>> = ({ navigation }) => {
+  const order = useAppSelector(selectItems);
+
   return (
     <View>
-      <Text>Cart Screen</Text>
-
-      <Button onPress={() => navigation.navigate('Menu')}>
-        {'Go to Menu'}
-      </Button>
+      <Text>{'Location Goes Here'}</Text>
+      <Text>{'Order Contains:'}</Text>
+      <FlatList
+        data={order}
+        renderItem={({ item, index }) => (
+          <Text key={`${item.id}-${index}`}>{item.name}</Text>
+        )}
+      />
+      <Text>{`Subtotal: ${order.reduce(
+        (sum, item) => +item.price + sum,
+        0
+      )}`}</Text>
     </View>
   );
 };
