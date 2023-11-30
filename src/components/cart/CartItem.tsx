756a30b2e75a606c287f5d3a6fc9f1a9d1c1b8bd
inferrinizzard <inferrinizzard@gmail.com>
2023-11-29 18:55:27 -0800

    add basic layout for cart item

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,5 +1,5 @@
-import { Image } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { Image, View } from 'react-native';
+import { Button, Card, Text } from 'react-native-paper';
 
 import { useAppSelector } from '../../redux/store';
 import { selectImages } from '../../redux/slices/dataSlice';
@@ -15,10 +15,25 @@ const CartItem: React.FC<CartItemProps> = ({ ...item }) => {
 
   return (
     <Card>
-      <Card.Content style={{ display: 'flex', flexDirection: 'row' }}>
-        <Image source={{ uri: imageUrl, height: 120, width: 160 }} />
-        <Text>{item.name}</Text>
-        <Text>{` $${item.price}`}</Text>
+      <Card.Content>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <Image source={{ uri: imageUrl, height: 120, width: 160 }} />
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+            <Text>{'Item Price and Calories'}</Text>
+            <Text>{'Item Details'}</Text>
+          </View>
+        </View>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <Button>
+            <Text>{'Edit'}</Text>
+          </Button>
+          <Button>
+            <Text>{'Remove'}</Text>
+          </Button>
+          <Text>{'Quantity'}</Text>
+        </View>
       </Card.Content>
     </Card>
   );
