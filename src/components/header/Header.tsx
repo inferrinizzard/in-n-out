9f7eeb8a1d6a50cc733fbecbde1e1fcf7c07eb55
inferrinizzard <inferrinizzard@gmail.com>
2023-12-13 07:13:23 -0800

    fix menu header for item screen

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,17 +6,21 @@ import {
   useRoute,
 } from '@react-navigation/native';
 
+import { useAppSelector } from '../../redux/store';
+import { selectActiveItem } from '../../redux/slices/orderSlice';
+
 export interface HeaderProps extends HeaderTitleProps {}
 
 const Header: React.FC<HeaderProps> = ({ tintColor, children }) => {
   const route = useRoute();
+  const activeItem = useAppSelector(selectActiveItem);
 
   const focusedRouteName = getFocusedRouteNameFromRoute(route);
 
   return (
     <View>
       <Text style={{ fontSize: 32, fontWeight: 'bold' }}>
-        {focusedRouteName || children}
+        {activeItem?.name ?? (focusedRouteName || children)}
       </Text>
     </View>
   );
