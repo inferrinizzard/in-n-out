48077334daa83d312919ebd468606eef06155cc9
inferrinizzard <inferrinizzard@gmail.com>
2025-02-08 13:46:20 -0800

    mvoe empty cart to own file

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -3,42 +3,41 @@ import { FlatList, View } from "react-native";
 import { Button, Divider, Text } from "react-native-paper";
 import { useAtom } from "jotai";
 
-import { type StackScreenProps, ScreenKeys } from "@src/navigation";
+import type { StackScreenProps, ScreenKeys } from "@src/navigation";
 import { orderAtom } from "@src/atoms/order.atom";
 import ScreenContainer from "@src/components/layout/ScreenContainer";
 
 import CartItem from "./components/CartItem";
 import CartLocation from "./components/CartLocation";
+import { EmptyCart } from "./components/EmptyCart";
 
 export interface CartProps extends StackScreenProps<typeof ScreenKeys.Cart> {}
 
 const Cart = ({ navigation }: CartProps) => {
-	const [order, orderSetter] = useAtom(orderAtom);
+	const [order, getOrderSetter] = useAtom(orderAtom);
 	const orderItems = useMemo(() => Object.entries(order), [order]);
+
+	const cartBody = useMemo(() => {
+		if (!orderItems.length) {
+			return <EmptyCart navigation={navigation} />;
+		}
+
+		return (
+			<FlatList
+				data={orderItems}
+				renderItem={({ item: [uuid, item] }) => (
+					<CartItem key={uuid} uuid={uuid} {...item} />
+				)}
+			/>
+		);
+	}, [orderItems, navigation]);
 
 	return (
 		<ScreenContainer>
 			<CartLocation />
 
 			<View style={{ flexGrow: 1, marginTop: 90 }}>
-				{orderItems.length ? (
-					<FlatList
-						data={orderItems}
-						renderItem={({ item: [uuid, item] }) => (
-							<CartItem key={uuid} uuid={uuid} {...item} />
-						)}
-					/>
-				) : (
-					<View>
-						<Text>{"No items in cart!"}</Text>
-						<Button
-							mode="contained"
-							onPress={() => navigation.replace(ScreenKeys.Menu)}
-						>
-							<Text>{"Add items"}</Text>
-						</Button>
-					</View>
-				)}
+				{cartBody}
 				{orderItems.length ? (
 					<View
 						style={{
