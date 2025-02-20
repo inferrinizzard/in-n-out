64b7a1a2f389a61b4acee1b8153ade5f7ff3539d
inferrinizzard <inferrinizzard@gmail.com>
2025-02-19 21:19:39 -0800

    add receipt

--- src/screens/Cart/components/PriceTotal.tsx
+++ src/screens/Cart/components/PriceTotal.tsx
@@ -1,13 +1,17 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Button } from "react-native-paper";
 import { useAtomValue } from "jotai";
 
 import { orderAtom } from "@src/atoms/order.atom";
 import { Box, Text } from "@src/components";
 
-const TAX_RATE = 10.25; // Francisquito -> Baldwin Park -> LA County
+import { Receipt } from "./Receipt";
+
+export const TAX_RATE = 10.25; // Francisquito -> Baldwin Park -> LA County
 
 export const PriceTotal = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const order = useAtomValue(orderAtom);
 	const orderItems = useMemo(() => Object.entries(order), [order]);
 
@@ -22,33 +26,42 @@ export const PriceTotal = () => {
 	const total = subtotal + taxesAndFees;
 
 	return (
-		<Box
-			gap="xs"
-			paddingHorizontal="m"
-			paddingVertical="s"
-			borderTopColor="greyLight"
-			borderTopWidth={1}
-		>
-			<Box flexDirection="row" justifyContent="space-between" padding="xs">
-				<Text>{"Subtotal:"}</Text>
-				<Text>{`$${subtotal.toFixed(2)}`}</Text>
-			</Box>
-			<Box flexDirection="row" justifyContent="space-between" padding="xs">
-				<Text>{"Taxes and Fees:"}</Text>
-				<Text>{`$${taxesAndFees.toFixed(2)}`}</Text>
-			</Box>
+		<>
 			<Box
-				flexDirection="row"
-				justifyContent="space-between"
-				padding="xs"
-				paddingBottom="s"
+				gap="xs"
+				paddingHorizontal="m"
+				paddingVertical="s"
+				borderTopColor="greyLight"
+				borderTopWidth={1}
 			>
-				<Text variant="bold">{"Total:"}</Text>
-				<Text variant="bold" letterSpacing={0}>{`$${total.toFixed(2)}`}</Text>
+				<Box flexDirection="row" justifyContent="space-between" padding="xs">
+					<Text>{"Subtotal:"}</Text>
+					<Text>{`$${subtotal.toFixed(2)}`}</Text>
+				</Box>
+				<Box flexDirection="row" justifyContent="space-between" padding="xs">
+					<Text>{"Taxes and Fees:"}</Text>
+					<Text>{`$${taxesAndFees.toFixed(2)}`}</Text>
+				</Box>
+				<Box
+					flexDirection="row"
+					justifyContent="space-between"
+					padding="xs"
+					paddingBottom="s"
+				>
+					<Text variant="bold">{"Total:"}</Text>
+					<Text variant="bold" letterSpacing={0}>{`$${total.toFixed(2)}`}</Text>
+				</Box>
+				<Button mode="contained" onPress={() => setIsModalOpen(true)}>
+					<Text variant="bold">{"Review and Pay"}</Text>
+				</Button>
 			</Box>
-			<Button mode="contained">
-				<Text variant="bold">{"Review and Pay"}</Text>
-			</Button>
-		</Box>
+			<Receipt
+				isVisible={isModalOpen}
+				onExit={() => setIsModalOpen(false)}
+				subtotal={subtotal}
+				taxesAndFees={taxesAndFees}
+				total={total}
+			/>
+		</>
 	);
 };
