11f0c0a211d159ec3ccd0c613daca707699616c8
inferrinizzard <inferrinizzard@gmail.com>
2025-02-20 07:59:04 -0800

    update header button

--- src/navigation/components/Header/HeaderButton.tsx
+++ src/navigation/components/Header/HeaderButton.tsx
@@ -1,15 +1,18 @@
 import { Button, Icon } from "react-native-paper";
+import { useAtomValue } from "jotai";
+
+import { queueAtom } from "@src/atoms/queue.atom";
 
 import type { HeaderProps } from "./index";
 
-export interface HeaderButtonProps extends Pick<HeaderProps, "navigation"> {
-	shouldUseX?: boolean;
-}
+export interface HeaderButtonProps extends Pick<HeaderProps, "navigation"> {}
 
-const HeaderButton = ({ navigation, shouldUseX }: HeaderButtonProps) => {
+const HeaderButton = ({ navigation }: HeaderButtonProps) => {
+	const { pending } = useAtomValue(queueAtom);
+
 	return (
 		<Button onPress={navigation.goBack}>
-			<Icon source={shouldUseX ? "close" : "chevron-left"} size={36} />
+			<Icon source={pending.length ? "chevron-left" : "close"} size={36} />
 		</Button>
 	);
 };
