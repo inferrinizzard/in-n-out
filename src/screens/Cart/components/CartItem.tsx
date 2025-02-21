68f8fb04c04ec6163fa9bab288ae2e9caa8f2043
inferrinizzard <inferrinizzard@gmail.com>
2025-02-21 08:39:38 -0800

    filter default val / count

--- src/screens/Cart/components/CartItem.tsx
+++ src/screens/Cart/components/CartItem.tsx
@@ -32,6 +32,11 @@ const CartItem = ({
 	const image = getImage(sku);
 	const itemText = getCopy(sku).toUpperCase();
 
+	const defaultOptions = {
+		...ItemOptionMap[item as keyof typeof ItemOptionMap].default,
+		...(SkuItemMap[sku] as { override?: object }).override,
+	};
+
 	const customisationData = (
 		Object.entries(options ?? {}) as [
 			OptionKey,
@@ -41,10 +46,6 @@ const CartItem = ({
 		if (!(item in ItemOptionMap)) {
 			return true;
 		}
-		const defaultOptions = {
-			...ItemOptionMap[item as keyof typeof ItemOptionMap].default,
-			...(SkuItemMap[sku] as { override?: object }).override,
-		};
 
 		if (!(optionKey in defaultOptions)) {
 			return true;
@@ -91,8 +92,17 @@ const CartItem = ({
 				<Text variant="header">{itemText}</Text>
 				{customisationData.map(([key, val]) => (
 					<React.Fragment key={`${uuid}-${key}`}>
-						<Text>{`${key}: ${val.value}`}</Text>
-						{"count" in val && <Text>{`${key}: ${val.count}`}</Text>}
+						{
+							// @ts-expect-error
+							defaultOptions[key].value !== val.value && (
+								<Text>{`${key}: ${val.value}`}</Text>
+							)
+						}
+						{"count" in val &&
+							// @ts-expect-error
+							defaultOptions[key].count !== val.count && (
+								<Text>{`${key}: ${val.count}`}</Text>
+							)}
 						{val.flags &&
 							Object.entries(val.flags).map(([flagKey, flagValue]) =>
 								flagValue ? <Text key={flagKey}>{`- ${flagKey}`}</Text> : null,
