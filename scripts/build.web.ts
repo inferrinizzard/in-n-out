367ab0cff42ce07b077834b120c8e30590cad5fb
inferrinizzard <inferrinizzard@gmail.com>
2025-03-24 08:23:07 -0700

    promisify

--- scripts/build.web.ts
+++ scripts/build.web.ts
@@ -1,13 +1,15 @@
 import { rm } from "node:fs/promises";
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
-import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import { exec as _exec } from "node:child_process";
+const exec = promisify(_exec);
 
 const appJsonString = readFileSync("./app.json").toString();
 const appJson = JSON.parse(appJsonString);
 const baseAppJson = JSON.parse(appJsonString);
 
 if (!process.env.CI) {
-	console.log(process.cwd());
+	console.log("Updating app.json");
 	if (existsSync("./dist")) {
 		rm("./dist", { recursive: true });
 	}
@@ -17,10 +19,11 @@ if (!process.env.CI) {
 	writeFileSync("./app.json", JSON.stringify(appJson));
 }
 
-exec("expo export -p web");
-
-if (!process.env.CI) {
-	writeFileSync("./app.json", JSON.stringify(baseAppJson));
-}
-
-exec("npx prettier --write app.json");
+exec("expo export -p web")
+	.then(() => {
+		if (!process.env.CI) {
+			console.log("Reverting app.json");
+			writeFileSync("./app.json", JSON.stringify(baseAppJson));
+		}
+	})
+	.finally(() => exec("npx prettier --write app.json"));
--- scripts/deploy.web.ts
+++ scripts/deploy.web.ts
@@ -1,16 +1,20 @@
-import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import { exec as _exec } from "node:child_process";
+const exec = promisify(_exec);
 
 const additionalArgs = process.argv.slice(2);
 
 let commitHash = "";
 
-exec("git log -1 --pretty=format:%h", (error, out, err) => {
-	commitHash = out;
-});
-
-exec(
-	[
-		`gh-pages --nojekyll -d dist -m "base: ${commitHash}"`,
-		...additionalArgs,
-	].join(" "),
-);
+exec("git log -1 --pretty=format:%h")
+	.then(({ stdout }) => {
+		commitHash = stdout;
+	})
+	.then(() =>
+		exec(
+			[
+				`gh-pages --nojekyll -d dist -m "base: ${commitHash}"`,
+				...additionalArgs,
+			].join(" "),
+		),
+	);
