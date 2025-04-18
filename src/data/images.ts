4d123dd16d0570417bbeaf26752dff3756d696d9
inferrinizzard <inferrinizzard@gmail.com>
2025-04-18 08:16:47 -0600

    add misc temp missing images

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,6 +1,7 @@
 import type { SkuKey } from "./sku";
 
 export const ImageData = {
+	// Items
 	DblDblCombo: require("@images/menu/DblDblCombo.png"),
 	CheeseburgerCombo: require("@images/menu/CheeseburgerCombo.png"),
 	HamburgerCombo: require("@images/menu/HamburgerCombo.png"),
@@ -34,6 +35,19 @@ export const ImageData = {
 	GrilledOnions: require("@images/temp/grilled_onions_TEMP.jpg"),
 	Pickles: require("@images/temp/pickles_TEMP.webp"),
 	Chilis: require("@images/temp/chilis_TEMP.jpg"),
+
+	AnimalStyle: require("@images/temp/AnimalStyle_TEMP.jpg"),
+	ProteinStyle: require("@images/temp/ProteinStyle_TEMP.jpg"),
+	"3X3": require("@images/temp/3X3_TEMP.jpg"),
+	"4X4": require("@images/temp/4X4_TEMP.jpg"),
+	AnimalFries: require("@images/temp/AnimalFries_TEMP.jpg"),
+	FlyingDutchman: require("@images/temp/FlyingDutchman_TEMP.jpg"),
+	WishBurger: require("@images/temp/WishBurger_TEMP.jpg"),
+	PupPatty: require("@images/temp/PupPatty_TEMP.jpg"),
+
+	Stickers: require("@images/temp/Stickers_TEMP.jpg"),
+	PaperHat: require("@images/temp/PaperHat_TEMP.jpg"),
+	GiftCard: require("@images/temp/GiftCard_TEMP.jpeg"),
 };
 
 ImageData satisfies Partial<Record<SkuKey, unknown>>;
